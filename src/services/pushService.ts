6c7198dda45ac2ef1faf6cf01b948277b8e6ef2c
Sammiehosty <samuelfrom1991@gmail.com>
2026-06-25 20:35:58 +0100

    Implement PushService for service worker registration

--- src/services/pushService.ts
+++ src/services/pushService.ts
@@ -1,1 +1,20 @@
+class PushService {
 
+    private registration: ServiceWorkerRegistration | null = null;
+
+    async init() {
+
+        if (!('serviceWorker' in navigator))
+            return false;
+
+        this.registration = await navigator.serviceWorker.register('/sw.js');
+
+        console.log("Service Worker Registered");
+
+        return true;
+
+    }
+
+}
+
+export default new PushService();
