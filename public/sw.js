0b342fcbe1d57d5a4bc1bf84266e7d48f62ddb5a
Sammiehosty <samuelfrom1991@gmail.com>
2026-06-25 20:29:49 +0100

    Add push and notification click event listeners
    Implement push notification handling in service worker.

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,49 @@
+self.addEventListener('push', event => {
 
+    const data = event.data.json();
+
+    event.waitUntil(
+
+        self.registration.showNotification(
+
+            data.title,
+
+            {
+
+                body: data.body,
+
+                icon: data.icon,
+
+                badge: data.badge,
+
+                image: data.image,
+
+                data: {
+
+                    url: data.url
+
+                }
+
+            }
+
+        )
+
+    );
+
+});
+
+self.addEventListener('notificationclick', event => {
+
+    event.notification.close();
+
+    event.waitUntil(
+
+        clients.openWindow(
+
+            event.notification.data.url
+
+        )
+
+    );
+
+});
