51da912df514a44ac1ab0b9614c07609a5307034
Sammiehosty <samuelfrom1991@gmail.com>
2026-06-26 21:29:39 +0100

    Update HomeView.tsx

--- src/components/HomeView.tsx
+++ src/components/HomeView.tsx
@@ -86,7 +86,7 @@ export default function HomeView({ onNavigate }: HomeViewProps) {
             latestData.sermons[0]
           );
 
-          setContinueProgress(42);
+          const progress = await getContinueListening();
 
         }
 
